8d54d36e5a02b1e056108aa426c3d9435fddd750
Luiz Felipe Duarte Elias <felipe15lfde@gmail.com>
2024-01-20 19:05:18 -0300

    Implementando o componente Estudo

--- src/main/java/br/com/felipeduarte/gerenciadordeestudo/controller/EstudoController.java
+++ src/main/java/br/com/felipeduarte/gerenciadordeestudo/controller/EstudoController.java
@@ -2,7 +2,13 @@ package br.com.felipeduarte.gerenciadordeestudo.controller;
 
 import java.net.URI;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,9 +17,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.felipeduarte.gerenciadordeestudo.controller.exceptions.ObjectNotFoundException;
 import br.com.felipeduarte.gerenciadordeestudo.dto.EstudoDTO;
 import br.com.felipeduarte.gerenciadordeestudo.dto.EstudoDadosDTO;
 import br.com.felipeduarte.gerenciadordeestudo.service.EstudoService;
+import br.com.felipeduarte.gerenciadordeestudo.service.exceptions.ObjectNotFoundFromParameterException;
 
 @RestController
 @RequestMapping(value =  "/api/v1/estudo")
@@ -25,7 +33,6 @@ public class EstudoController {
 		this.service = service;
 	}
 	
-	/*
 	@PostMapping
 	public ResponseEntity<EstudoDTO> cadastrar(@RequestBody EstudoDadosDTO dados, UriComponentsBuilder uriBuilder) {
 	
@@ -43,10 +50,54 @@ public class EstudoController {
 		
 		try {
 			
-		}catch(Exception ex) {
+			EstudoDTO estudo = this.service.alterar(idEstudo, dados);
 			
+			return ResponseEntity.ok(estudo);
+			
+		}catch(ObjectNotFoundFromParameterException ex) {
+			throw new ObjectNotFoundException(ex.getMessage());
 		}
 		
-	}*/
+	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<EstudoDTO> buscarPorId(@PathVariable(name = "id") Long idEstudo){
+		
+		try {
+			
+			EstudoDTO estudo = this.service.buscarPorId(idEstudo);
+			
+			return ResponseEntity.ok(estudo);
+			
+		}catch(ObjectNotFoundFromParameterException ex) {
+			throw new ObjectNotFoundException(ex.getMessage());
+		}
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> excluir(@PathVariable(name = "id") Long idEstudo){
+		
+		try {
+			
+			this.service.excluir(idEstudo);
+			
+			return ResponseEntity.ok().build();
+			
+		}catch(ObjectNotFoundFromParameterException ex) {
+			throw new ObjectNotFoundException(ex.getMessage());
+		}
+		
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<EstudoDTO>> listar(
+			@PageableDefault(page = 0, size = 5, direction = Direction.ASC, sort = "nome") Pageable paginacao){
+		
+		Page<EstudoDTO> pagina = this.service.listar(paginacao);
+		
+		return ResponseEntity.ok(pagina);
+		
+	}
+ 	
 }
--- src/main/java/br/com/felipeduarte/gerenciadordeestudo/service/EstudoService.java
+++ src/main/java/br/com/felipeduarte/gerenciadordeestudo/service/EstudoService.java
@@ -1,11 +1,16 @@
 package br.com.felipeduarte.gerenciadordeestudo.service;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.felipeduarte.gerenciadordeestudo.dto.EstudoDTO;
 import br.com.felipeduarte.gerenciadordeestudo.dto.EstudoDadosDTO;
 import br.com.felipeduarte.gerenciadordeestudo.entity.Estudo;
 import br.com.felipeduarte.gerenciadordeestudo.repository.EstudoRepository;
+import br.com.felipeduarte.gerenciadordeestudo.service.exceptions.ObjectNotFoundFromParameterException;
 
 @Service
 public class EstudoService {
@@ -16,7 +21,6 @@ public class EstudoService {
 		this.repository = repository;
 	}
 
-	/*
 	public EstudoDTO cadastrar(EstudoDadosDTO dados) {
 		
 		Estudo estudo = new Estudo(dados);
@@ -25,6 +29,50 @@ public class EstudoService {
 		
 		return new EstudoDTO(estudo);
 	
-	}*/
+	}
+
+	public EstudoDTO alterar(Long idEstudo, EstudoDadosDTO dados) {
+		
+		Optional<Estudo> optEstudo = this.repository.findById(idEstudo);
+		
+		if(optEstudo.isEmpty()) throw new ObjectNotFoundFromParameterException("Erro! Estudo não encontrado!");
+		
+		Estudo estudo = optEstudo.get();
+		estudo.setNome(dados.getNome());
+		estudo.setDescricao(dados.getDescricao());
+		
+		estudo = this.repository.save(estudo);
+		
+		return new EstudoDTO(estudo);
+		
+	}
+
+	public EstudoDTO buscarPorId(Long idEstudo) {
+		
+		Optional<Estudo> optEstudo = this.repository.findById(idEstudo);
+		
+		if(optEstudo.isEmpty()) throw new ObjectNotFoundFromParameterException("Erro! Estudo não encontrado!");
+		
+		return new EstudoDTO(optEstudo.get());
+		
+	}
+
+	public void excluir(Long idEstudo) {
+		
+		Optional<Estudo> optEstudo = this.repository.findById(idEstudo);
+		
+		if(optEstudo.isEmpty()) throw new ObjectNotFoundFromParameterException("Erro! Estudo não encontrado!");
+		
+		this.repository.delete(optEstudo.get());
+		
+	}
+
+	public Page<EstudoDTO> listar(Pageable paginacao) {
+		
+		Page<Estudo> paginaEstudos =  this.repository.findAll(paginacao);
+		
+		return paginaEstudos.map(EstudoDTO::new);
+		
+	}
 	
 }
