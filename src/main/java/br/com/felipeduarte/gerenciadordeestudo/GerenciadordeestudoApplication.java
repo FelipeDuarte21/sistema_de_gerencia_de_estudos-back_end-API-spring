6b60d0783cfce09ed76d6f9cda5de113bbdc466c
Luiz Felipe Duarte Elias <felipe15lfde@gmail.com>
2024-01-20 00:15:36 -0300

    Subindo os arquivos base da API

---
+++ src/main/java/br/com/felipeduarte/gerenciadordeestudo/GerenciadordeestudoApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipeduarte.gerenciadordeestudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadordeestudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadordeestudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/felipeduarte/gerenciadordeestudo/GerenciadordeestudoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipeduarte.gerenciadordeestudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadordeestudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
